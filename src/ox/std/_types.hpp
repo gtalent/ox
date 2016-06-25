1c428aa1f301000781ab6297aa05ed5be2cb59c4
Gary Talent <gtalent2@gmail.com>
2016-06-24 20:43:07 -0500

    Added support for ARM.

--- src/ox/std/_types.hpp
+++ src/ox/std/_types.hpp
@@ -10,21 +10,21 @@
 namespace ox {
 namespace std {
 
-typedef  char               int8_t;
-typedef  unsigned char      uint8_t;
-typedef  short              int16_t;
-typedef  unsigned short     uint16_t;
-typedef  int                int32_t;
-typedef  unsigned int       uint32_t;
-typedef  unsigned           uint_t;
-typedef  long long          int64_t;
-typedef  unsigned long long uint64_t;
+typedef char               int8_t;
+typedef unsigned char      uint8_t;
+typedef short              int16_t;
+typedef unsigned short     uint16_t;
+typedef int                int32_t;
+typedef unsigned int       uint32_t;
+typedef unsigned           uint_t;
+typedef long long          int64_t;
+typedef unsigned long long uint64_t;
 
-typedef  uint32_t Error;
+typedef uint32_t Error;
 
-#if defined(_LP64) || defined(__ppc64__) || defined(_WIN64)
+#if defined(_LP64) || defined(__ppc64__) || defined(_WIN64) || defined(__aarch64__)
 typedef uint64_t size_t;
-#elif defined(_LP32) || defined(__ppc__) || defined(_WIN32)
+#elif defined(_LP32) || defined(__ppc__) || defined(_WIN32) || defined(__arm__)
 typedef uint32_t size_t;
 #else
 #error size_t undefined
