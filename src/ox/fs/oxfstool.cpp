7f583c8709d69450e0229df08d6dfbf236069722
Gary Talent <gtalent2@gmail.com>
2016-07-03 17:04:04 -0500

    Add include that is needed for Linux builds

--- src/ox/fs/oxfstool.cpp
+++ src/ox/fs/oxfstool.cpp
@@ -5,6 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ox/std/strops.hpp>
