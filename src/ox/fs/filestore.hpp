565c56eeecc9fc5457fc8931a8593b302135b6b5
Gary Talent <gtalent2@gmail.com>
2017-10-17 20:51:04 -0500

    Increment FileStore version for inode ID size change

--- src/ox/fs/filestore.hpp
+++ src/ox/fs/filestore.hpp
@@ -17,7 +17,7 @@ struct __attribute__((packed)) FileStoreHeader {
 	public:
 		typedef InodeId InodeId_t;
 		typedef FsT FsSize_t;
-		const static auto VERSION = 6;
+		const static auto VERSION = 7;
 
 	private:
 		uint16_t m_version;
